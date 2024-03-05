7fb97f6c3166eb54617ee2ce062e2b4b9b00ebbf
Christopher Curley <clevercurley@gmail.com>
2024-03-05 13:43:36 -0500

    Added bouncing animation to the icon in intro component.

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faHandPointDown } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrl: './intro.component.css'
+  styleUrl: './intro.component.css',
 })
 export class IntroComponent {
-
+  faHandPointDown = faHandPointDown;
 }
