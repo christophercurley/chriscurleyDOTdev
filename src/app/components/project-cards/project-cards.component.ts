117fe15bbdd4df4fdb7362a57c44726ea7d4fc6b
Christopher Curley <clevercurley@gmail.com>
2024-03-06 07:17:41 -0500

    Complete basic styling and layout for project card.

--- src/app/components/project-cards/project-cards.component.ts
+++ src/app/components/project-cards/project-cards.component.ts
@@ -11,10 +11,26 @@ export class ProjectCardsComponent {
     {
       name: 'ngFlix',
       url: 'https://ngflix.chriscurley.dev',
+      screenshotPath: '../../../assets/screenshots/ngflix.png',
+      logoPath: '../../../assets/logos/ngflixLogo.png',
+      description:
+        "Browse and discover movies and TV shows! Built with Angular 17 and plugged into TMDB's API.",
+    },
+    {
+      name: 'Calculator',
+      url: 'calculator',
       screenshotPath: '',
       logoPath: '',
       description:
-        "Browse and discover movies and TV shows! Built with Angular 17 and plugged into TMDB's database.",
+        'A clone of the iOS calculator, created with only Vanilla JS and HTML/CSS.',
+    },
+    {
+      name: 'www.chriscurley.dev',
+      url: '/',
+      screenshotPath: '../../../assets/screenshots/chriscurleyDOTdev.png',
+      logoPath: '',
+      description:
+        'Look familiar?! Just a place for me to showcase some projects and link my socials :)',
     },
   ];
 }
