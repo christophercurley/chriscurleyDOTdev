aa92051702adafbae8cca9badef56633ec604c60
Christopher Curley <clevercurley@gmail.com>
2024-03-05 19:26:51 -0500

    Remove all traces of carousel from scratch. Return from the brink.

--- src/app/components/project-cards/project-cards.component.ts
+++ src/app/components/project-cards/project-cards.component.ts
@@ -1,54 +1,8 @@
-import {
-  Component,
-  ElementRef,
-  HostListener,
-  Renderer2,
-  ViewChild,
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-project-cards',
   templateUrl: './project-cards.component.html',
   styleUrl: './project-cards.component.css',
 })
-export class ProjectCardsComponent {
-  @ViewChild('card') card!: ElementRef;
-  isDragged = false;
-  lastX: number = 0;
-
-  constructor(private renderer: Renderer2) {}
-
-  onMouseDown(event: MouseEvent): void {
-    this.renderer.addClass(this.card.nativeElement, 'grabbing');
-    this.isDragged = true;
-    this.lastX = event.clientX;
-    console.log(this.isDragged, this.lastX);
-  }
-
-  @HostListener('document:mouseup')
-  onMouseUp(): void {
-    this.isDragged = false;
-    if (this.card.nativeElement.classList.contains('grabbing')) {
-      this.renderer.removeClass(this.card.nativeElement, 'grabbing');
-    }
-  }
-
-  @HostListener('document:mousemove', ['$event'])
-  onMouseMove(event: MouseEvent) {
-    if (!this.isDragged) return;
-
-    // calculate distance moved
-    const dx = event.clientX - this.lastX;
-    this.lastX = event.clientX;
-
-    const style = window.getComputedStyle(this.card.nativeElement);
-    const matrix = new WebKitCSSMatrix(style.transform);
-    const newX = matrix.m41 + dx;
-
-    this.renderer.setStyle(
-      this.card.nativeElement,
-      'transform',
-      `translateX(${newX}px)`
-    );
-  }
-}
+export class ProjectCardsComponent {}
